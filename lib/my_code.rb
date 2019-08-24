0693edc274d6f5429a81f65c3241f26acb3baddc
Josh <cheukhinjoshua.ng@gmail.com>
2019-08-24 16:09:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(source_array, starting_point = 0)
   new = 0
   i = 0
   while i < source_array.length do
-    new = new + source_array[i]
+    yield(source_array[i])
     i += 1
   end
   return new + starting_point
